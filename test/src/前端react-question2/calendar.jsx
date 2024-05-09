001979e714757b47d0b01f0e5a290a5419c4f3fa
何喻寶 <qazwsx6120p@gmail.com>
2024-05-09 22:43:38 +0800

    update:優化

--- test/src/前端react-question2/calendar.jsx
+++ test/src/前端react-question2/calendar.jsx
@@ -1,6 +1,4 @@
-// Calendar.js
-
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import moment from "moment";
 import "../styles/calendar.css";
 import CalendarTitle from "./components/calendarTitle";
@@ -24,89 +22,48 @@ const Calendar = ({ date }) => {
 
   /** 儲存所選範圍 */
   const [selectedRange, setSelectedRange] = useState([]);
+
   // ===================== 函數 =====================
 
   /** 處理點擊上一個月的事件 */
-  const handlePreviousMonth = () => {
+  const handlePreviousMonth = useCallback(() => {
     setSelectedDate((prevDate) => prevDate.clone().subtract(1, "month"));
-  };
+  }, []);
 
   /** 處理點擊下一個月的事件 */
-  const handleNextMonth = () => {
+  const handleNextMonth = useCallback(() => {
     setSelectedDate((prevDate) => prevDate.clone().add(1, "month"));
-  };
+  }, []);
 
   /** 處理日期點擊事件，並且設置使用者選擇的範圍
    *  @param day 日曆的日期 */
-  const handleDayClick = (day) => {
-    if (!day.inCurrentMonth) return; // 如果是非本月日期，則返回
+  const handleDayClick = useCallback(
+    (day) => {
+      if (!day.inCurrentMonth) return;
 
-    let newRange = [];
+      let newRange = [];
 
-    // 檢查是否已選擇範圍的第一個日期
-    if (selectedRange.length === 0) {
-      newRange = [day.day];
-    }
-    // 檢查是否已選擇範圍的第二個日期
-    else if (selectedRange.length === 1) {
-      const startDay = selectedRange[0];
-      const endDay = day.day;
+      if (selectedRange.length === 0) {
+        newRange = [day.day];
+      } else if (selectedRange.length === 1) {
+        const startDay = selectedRange[0];
+        const endDay = day.day;
 
-      // 確保較小的日期作為範圍的起始點，較大的日期作為範圍的終點
-      if (startDay < endDay) {
-        for (let i = startDay; i <= endDay; i++) {
+        for (
+          let i = Math.min(startDay, endDay);
+          i <= Math.max(startDay, endDay);
+          i++
+        ) {
           newRange.push(i);
         }
       } else {
-        for (let i = endDay; i <= startDay; i++) {
-          newRange.push(i);
-        }
+        newRange = [];
       }
-    }
-    // 如果已經選擇了兩個日期，則清空選擇範圍
-    else {
-      newRange = [];
-    }
-    setSelectedRange(newRange);
-  };
 
-  /** 渲染日曆上的日期 */
-  const renderCalendarCells = () => {
-    return [...Array(CALENDAR_HAS_FIVE_WEEKS)].map((_, weekIndex) => (
-      <tr key={weekIndex}>
-        {[...Array(DAYS_IN_WEEK)].map((_, dayIndex) => {
-          const index = weekIndex * DAYS_IN_WEEK + dayIndex;
-          const item = calendarDays[index];
-          if (item) {
-            /** 是否為非本月日期 */
-            const isNonCurrentMonthDay = !item.inCurrentMonth;
-
-            /** 是否為本月日期 */
-            const isToday =
-              item.day === today.date() &&
-              item.month === today.month() + 1 &&
-              item.year === today.year();
-
-            /** 是否為使用者選擇的本月日期範圍 */
-            const isSelected =
-              selectedRange.includes(item.day) && !isNonCurrentMonthDay;
-
-            return (
-              <td
-                key={index}
-                className={`calendar-day text-center ${
-                  isNonCurrentMonthDay ? "non-current-month" : ""
-                } ${isToday ? "today" : ""} ${isSelected ? "selected" : ""}`}
-                onClick={() => handleDayClick(item)}
-              >
-                {`${item.day} 日`}
-              </td>
-            );
-          }
-        })}
-      </tr>
-    ));
-  };
+      setSelectedRange(newRange);
+    },
+    [selectedRange]
+  );
 
   //============== 計算這個月的相關資訊 ==============
 
@@ -125,9 +82,6 @@ const Calendar = ({ date }) => {
   /** 本月的總天數 */
   const totalDaysInMonth = lastDayOfMonth.date();
 
-  /** 日曆日期陣列 */
-  const calendarDays = [];
-
   //============== 計算上或下個月的相關資訊 ==============
 
   /** 上個月 */
@@ -139,40 +93,98 @@ const Calendar = ({ date }) => {
   /** 上個月需要顯示的尾月日期數量 */
   const previousMonthDaysToShow = startDayOfWeek === 0 ? 6 : startDayOfWeek - 1;
 
-  /** 添加上個月的尾月日期到日曆中 */
-  for (let i = 0; i < previousMonthDaysToShow; i++) {
-    const day = daysInPreviousMonth - i;
-    calendarDays.unshift({
-      day: day,
-      month: previousMonth.month() + 1,
-      year: previousMonth.year(),
-      inCurrentMonth: false,
-    });
-  }
+  /** 處理要渲染的日曆天數陣列
+   *  @return 日曆天數陣列 */
+  const calendarDays = useMemo(() => {
+    const days = [];
 
-  /** 計算需要顯示的本月日期數量 */
-  const totalDaysToShow =
-    ALL_TD_COUNT - (previousMonthDaysToShow + totalDaysInMonth);
+    /** 添加上個月的尾月日期到日曆中 */
+    for (let i = 0; i < previousMonthDaysToShow; i++) {
+      const day = daysInPreviousMonth - i;
+      days.unshift({
+        day: day,
+        month: previousMonth.month() + 1,
+        year: previousMonth.year(),
+        inCurrentMonth: false,
+      });
+    }
 
-  /** 添加本月的日期到日曆中 */
-  for (let i = 1; i <= totalDaysInMonth; i++) {
-    calendarDays.push({
-      day: i,
-      month: firstDayOfMonth.month() + 1,
-      year: firstDayOfMonth.year(),
-      inCurrentMonth: true,
-    });
-  }
+    /** 添加本月的日期到日曆中 */
+    for (let i = 1; i <= totalDaysInMonth; i++) {
+      days.push({
+        day: i,
+        month: firstDayOfMonth.month() + 1,
+        year: firstDayOfMonth.year(),
+        inCurrentMonth: true,
+      });
+    }
+    /** 計算需要顯示的本月日期數量 */
+    const totalDaysToShow =
+      ALL_TD_COUNT - (previousMonthDaysToShow + totalDaysInMonth);
 
-  /** 添加下個月的日期到日曆中 */
-  for (let i = 1; i <= totalDaysToShow; i++) {
-    calendarDays.push({
-      day: i,
-      month: lastDayOfMonth.month() + 1,
-      year: lastDayOfMonth.year(),
-      inCurrentMonth: false,
-    });
-  }
+    /** 添加下個月的日期到日曆中 */
+    for (let i = 1; i <= totalDaysToShow; i++) {
+      days.push({
+        day: i,
+        month: lastDayOfMonth.month() + 1,
+        year: lastDayOfMonth.year(),
+        inCurrentMonth: false,
+      });
+    }
+
+    return days;
+  }, [
+    firstDayOfMonth,
+    lastDayOfMonth,
+    previousMonth,
+    daysInPreviousMonth,
+    previousMonthDaysToShow,
+    totalDaysInMonth,
+  ]);
+
+  /** 渲染日曆上的日期 */
+  const renderCalendarCells = useMemo(() => {
+    return (
+      <>
+        {[...Array(CALENDAR_HAS_FIVE_WEEKS)].map((_, weekIndex) => (
+          <tr key={weekIndex}>
+            {[...Array(DAYS_IN_WEEK)].map((_, dayIndex) => {
+              /** 索引 */
+              const index = weekIndex * DAYS_IN_WEEK + dayIndex;
+
+              /** 日曆值 */
+              const item = calendarDays[index];
+
+              /** 是否為非本月日期 */
+              const isNonCurrentMonthDay = !item.inCurrentMonth;
+
+              /** 是否為本月日期 */
+              const isToday =
+                item.day === today.date() &&
+                item.month === today.month() + 1 &&
+                item.year === today.year();
+
+              /** 是否為使用者選擇的本月日期範圍 */
+              const isSelected =
+                selectedRange.includes(item.day) && !isNonCurrentMonthDay;
+              return (
+                <td
+                  key={index}
+                  className={`calendar-day text-center 
+                  ${isNonCurrentMonthDay ? "non-current-month" : ""} 
+                  ${isToday ? "today" : ""} 
+                  ${isSelected ? "selected" : ""}`}
+                  onClick={() => handleDayClick(item)}
+                >
+                  {`${item.day} 日`}
+                </td>
+              );
+            })}
+          </tr>
+        ))}
+      </>
+    );
+  }, [calendarDays, handleDayClick, selectedRange, today]);
 
   return (
     <main className="main d-flex justify-content-center align-items-center">
@@ -183,10 +195,11 @@ const Calendar = ({ date }) => {
           handleNextMonth={handleNextMonth}
         />
         <table className="calendar-table">
-          <tbody>{renderCalendarCells()}</tbody>
+          <tbody>{renderCalendarCells}</tbody>
         </table>
       </div>
     </main>
   );
 };
+
 export default Calendar;
